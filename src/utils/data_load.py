c3099a27307c86ac834dd378021c883a5b6b2bb8
Matthew Segal <mattdsegal@gmail.com>
2019-10-09 11:13:57 +1100

    fix bug in combined data loader again

--- src/utils/data_load.py
+++ src/utils/data_load.py
@@ -6,37 +6,20 @@ class CombinedDataLoader:
     def __init__(self, loader_a, loader_b):
         self.loader_a = loader_a
         self.loader_b = loader_b
-        self.iter_a = loader_a.__iter__()
-        self.iter_b = loader_b.__iter__()
-        self.a_finished = False
-        self.b_finished = False
-        self.count = -1
 
     @property
     def is_loader_a(self):
         return self.count % 2 == 0
 
     def __len__(self):
-        return len(self.loader_a) + len(self.loader_b)
+        return min([2 * len(self.loader_a), 2 * len(self.loader_b)])
 
     def __iter__(self):
+        self.count = -1
+        self.iter_a = loader_a.__iter__()
+        self.iter_b = loader_b.__iter__()
         return self
 
     def __next__(self):
         self.count += 1
-        if self.is_loader_a and not self.a_finished:
-            try:
-                sample = next(self.iter_a)
-                return sample
-            except StopIteration:
-                self.a_finished = False
-        elif self.a_finished or not self.is_loader_a:
-            try:
-                sample = next(self.iter_b)
-                return sample
-            except StopIteration:
-                self.b_finished = False
-
-        if self.a_finished and self.b_finished:
-            raise StopIteration()
-
+        return next(self.iter_a) if self.is_loader_a else next(self.iter_b)
