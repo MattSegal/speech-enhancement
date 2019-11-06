b7e2959e9b919402c661cd823fa487a02144400b
Matthew Segal <mattdsegal@gmail.com>
2019-11-06 22:27:59 +1100

    Make S3 copy quiet

--- src/utils/s3.py
+++ src/utils/s3.py
@@ -8,7 +8,7 @@ def fetch_data(bucket_dir, target_dir):
     Recursively copy files from bucket dir to target dir
     """
     subprocess.run(
-        args=[f"aws s3 cp --recursive s3://{DATA_S3_BUCKET}/{bucket_dir} {target_dir}"],
+        args=[f"aws s3 cp --recursive --quiet s3://{DATA_S3_BUCKET}/{bucket_dir} {target_dir}"],
         shell=True,
         check=True,
     )
