c9d818d46c7869008b347011a6af0ad93f5bb8a4
Matthew Segal <mattdsegal@gmail.com>
2019-11-08 14:31:03 +1100

    Prefer G4DN to G3S when training

--- scripts/infra/settings.py
+++ scripts/infra/settings.py
@@ -8,5 +8,5 @@ EC2_INSTANCE_TYPE_LIMIT = 2
 EC2_AMI = "ami-05fbded987de0bcb0"
 EC2_SPOT_MAX_PRICE = "0.75"
 EC2_SECURITY_GROUP = "sg-3d0ecf44"
-EC2_LAUNCH_PREFERENCE = [EC2InstanceType.g3s, EC2InstanceType.g4dn, EC2InstanceType.p2]
+EC2_LAUNCH_PREFERENCE = [EC2InstanceType.g4dn, EC2InstanceType.g3s, EC2InstanceType.p2]
 AWS_REGION = "ap-southeast-2"
