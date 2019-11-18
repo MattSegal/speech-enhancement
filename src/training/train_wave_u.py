d7bef5faed80d1170f20c37e1c6d0946ff914779
Matthew Segal <mattdsegal@gmail.com>
2019-11-18 19:43:08 +1100

    Try half as much data

--- src/training/train_wave_u.py
+++ src/training/train_wave_u.py
@@ -9,9 +9,7 @@ import wandb
 
 from ..datasets import NoisySpeechDataset
 from ..models.wave_u_net import WaveUNet
-from ..models.mel_discriminator import MelDiscriminatorNet
 from ..utils.trackers import MovingAverage
-from ..utils.loss import l1_loss, LeastSquaresLoss
 from ..utils import checkpoint
 from ..utils.log import log_training_info
 
@@ -23,8 +21,6 @@ WANDB_PROJECT = "wave-u-net"
 LEARNING_RATE = 1e-4
 ADAM_BETAS = (0.5, 0.9)
 WEIGHT_DECAY = 1e-4
-DISC_WEIGHT = 1e-1
-DISC_LEARNING_RATE = 4 * LEARNING_RATE
 
 
 def train(num_epochs, use_cuda, batch_size, wandb_name, subsample, checkpoint_epochs):
@@ -62,18 +58,7 @@ def train(num_epochs, use_cuda, batch_size, wandb_name, subsample, checkpoint_ep
         net.parameters(), lr=LEARNING_RATE, betas=ADAM_BETAS, weight_decay=WEIGHT_DECAY
     )
 
-    # Initialize discriminator loss function, optimizer
-    disc_net = MelDiscriminatorNet().cuda() if use_cuda else MelDiscriminatorNet().cpu()
-
-    disc_net.train()
-    gan_loss = LeastSquaresLoss(disc_net)
-    optimizer_disc = optim.AdamW(disc_net.parameters(), lr=DISC_LEARNING_RATE, betas=ADAM_BETAS)
-
     # Keep track of loss history using moving average
-    disc_loss = MovingAverage(decay=0.8)
-    gen_loss = MovingAverage(decay=0.8)
-    training_loss = MovingAverage(decay=0.8)
-    validation_loss = MovingAverage(decay=0.8)
     mean_squared_error = nn.MSELoss()
     training_mse = MovingAverage(decay=0.8)
     validation_mse = MovingAverage(decay=0.8)
@@ -112,36 +97,15 @@ def train(num_epochs, use_cuda, batch_size, wandb_name, subsample, checkpoint_ep
 
             # Run loss function on over the model's prediction
             assert targets.shape == (batch_size, audio_length)
-            l1_loss_t = l1_loss(outputs, targets)
-
-            # Add discriminator to loss function
-            fake_audio = outputs.view(batch_size, 1, -1)
-            gen_gan_loss = gan_loss.for_generator(inputs, fake_audio)
-
-            loss = l1_loss_t + DISC_WEIGHT * gen_gan_loss
+            loss = mean_squared_error(outputs, targets)
 
             # Calculate model weight gradients from the loss and update model.
             loss.backward()
             optimizer.step()
 
             # Track training information
-            loss_amount = l1_loss_t.data.item()
-            training_loss.update(loss_amount)
-            mse = mean_squared_error(outputs, targets).data.item()
+            mse = loss.data.item()
             training_mse.update(mse)
-            loss_amount = gen_gan_loss.data.item()
-            gen_loss.update(loss_amount)
-
-            # Train discriminator
-            optimizer_disc.zero_grad()
-            fake_audio = outputs.view(batch_size, 1, -1).detach()
-            disc_gan_loss = gan_loss.for_discriminator(inputs, fake_audio)
-            disc_gan_loss.backward()
-            optimizer_disc.step()
-
-            # Track disc training information
-            loss_amount = disc_gan_loss.data.item()
-            disc_loss.update(loss_amount)
 
         # Check performance (loss) on validation set.
         net.eval()
@@ -154,21 +118,11 @@ def train(num_epochs, use_cuda, batch_size, wandb_name, subsample, checkpoint_ep
                 targets = targets.cuda() if use_cuda else targets.cpu()
                 outputs = net(inputs)
                 outputs = outputs.squeeze(dim=1)
-                loss = l1_loss(outputs, targets)
-                loss_amount = loss.data.item()
-                validation_loss.update(loss_amount)
                 mse = mean_squared_error(outputs, targets).data.item()
                 validation_mse.update(mse)
 
         log_training_info(
-            {
-                "Training L1 Loss": training_loss.value,
-                "Validation L1 Loss": validation_loss.value,
-                "Training Loss": training_mse.value,
-                "Validation Loss": validation_mse.value,
-                "Discriminator Loss": disc_loss.value,
-                "Generator Loss": gen_loss.value,
-            },
+            {"Training Loss": training_mse.value, "Validation Loss": validation_mse.value},
             use_wandb=use_wandb,
         )
 
