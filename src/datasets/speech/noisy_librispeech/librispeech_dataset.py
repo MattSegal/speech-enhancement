e5dcb7a3e797d4bfb32bf6b8fae197a9816fa6eb
Matthew Segal <mattdsegal@gmail.com>
2019-11-25 19:39:05 +1100

    Fix chunk subsample edge case

--- src/datasets/speech/noisy_librispeech/librispeech_dataset.py
+++ src/datasets/speech/noisy_librispeech/librispeech_dataset.py
@@ -28,9 +28,7 @@ class NoisyLibreSpeechDataset(S3BackedDataset):
         itr = list if self.quiet else tqdm
         self.clean_data = []
         self.clean_folder = os.path.join(self.data_path, f"{dataset_label}_set")
-        self.clean_filenames = self.find_flac_filenames(
-            self.clean_folder, subsample=subsample
-        )
+        self.clean_filenames = self.find_flac_filenames(self.clean_folder, subsample=subsample)
         if not quiet:
             print(f"Loading {dataset_label} dataset into memory.")
             print("Loading clean data...")
@@ -78,5 +76,5 @@ def subsample_chunk_random(tensor, chunk_width):
     """
     size = tensor.nelement()
     assert chunk_width < size
-    chunk_start = random.randint(0, size - chunk_width + 1)
+    chunk_start = random.randint(0, size - chunk_width)
     return tensor[chunk_start : chunk_start + chunk_width]
