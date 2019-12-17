1e198cb3746e82f4ffca6af2e14b38e7d3a4edc3
Matthew Segal <mattdsegal@gmail.com>
2019-12-17 16:12:29 +1100

    Normalize mel spectral input

--- src/datasets/speech/noisy_speech/speech_dataset_spectral.py
+++ src/datasets/speech/noisy_speech/speech_dataset_spectral.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from librosa.util import normalize
 
 from src.utils import spectral
 
@@ -13,11 +14,14 @@ class NoisySpectralSpeechDataset(NoisySpeechDataset):
     Get item by integer index,
     """
 
-    def process_sample(self, sample_arr):
+    def process_sample(self, sample_arr, norm):
         # Convert audio array to mel spectrum
         sample_spec = spectral.audio_to_log_mel_spec(sample_arr)
         # Knock off last time step so we have all dims as powers of 2
         sample_spec = sample_spec[:, :-1]
+        if norm:
+            sample_spec = normalize(sample_spec)
+
         # Add channel dimension
         sample_spec = np.expand_dims(sample_spec, axis=0)
         return torch.tensor(sample_spec)
@@ -26,6 +30,6 @@ class NoisySpectralSpeechDataset(NoisySpeechDataset):
         """
         Returns noisy and clean log magnitude spectrograms
         """
-        noisy_spectral = self.process_sample(self.noisy_data[idx])
-        clean_spectral = self.process_sample(self.clean_data[idx])
+        noisy_spectral = self.process_sample(self.noisy_data[idx], norm=True)
+        clean_spectral = self.process_sample(self.clean_data[idx], norm=False)
         return noisy_spectral, clean_spectral
